edd5953a4a2996dfd5c7f8fc6191e4c2a78a04b0
theadg <drewndrei@gmail.com>
2022-08-09 22:46:33 +0800

    Add additional screens and colors in config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ['./*.html'], //where to look for the utility classes
   theme: {
-    extend: {},
+    screens:{
+      sm: '375px',
+      lg: '1440px',
+    },
+    extend: {
+      colors: {
+        lightGray: 'hsl(212, 45%, 89%)',
+        grayishBlue: 'hsl(220, 15%, 89%)',
+        darkBlue: 'hsl(218, 44%, 22%)',
+      },
+      
+    },
   },
   plugins: [],
 }
