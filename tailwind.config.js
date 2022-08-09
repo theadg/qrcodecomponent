d03dfda5e35c1d2a28b0a2c024297e0bab3bfad4
theadg <drewndrei@gmail.com>
2022-08-09 23:02:31 +0800

    Add new font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,13 +6,16 @@ module.exports = {
       sm: '375px',
       lg: '1440px',
     },
+    fontFamily:{
+      'outfit' : ['Outfit', 'sans-serif'],
+    },
     extend: {
       colors: {
         lightGray: 'hsl(212, 45%, 89%)',
         grayishBlue: 'hsl(220, 15%, 89%)',
         darkBlue: 'hsl(218, 44%, 22%)',
       },
-      
+
     },
   },
   plugins: [],
