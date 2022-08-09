eb6606c8986c7b79db957e89f5860231109462bb
theadg <drewndrei@gmail.com>
2022-08-10 01:00:15 +0800

    Add necessary styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,17 +2,36 @@
 module.exports = {
   content: ['./*.html'], //where to look for the utility classes
   theme: {
+    container:{
+      width:{
+        DEFAULT: '100px',
+      },
+    },
+
     screens:{
       sm: '375px',
       lg: '1440px',
     },
+
     fontFamily:{
       'outfit' : ['Outfit', 'sans-serif'],
     },
+
+
+    borderRadius: {
+      'large': '16px',
+    },
+
+    
     extend: {
+      height: {
+        '78': '280px',
+        '128': '32rem',
+        '256': '256rem',
+      },
       colors: {
         lightGray: 'hsl(212, 45%, 89%)',
-        grayishBlue: 'hsl(220, 15%, 89%)',
+        grayishBlue: 'hsl(220, 45%, 89%)',
         darkBlue: 'hsl(218, 44%, 22%)',
       },
 
